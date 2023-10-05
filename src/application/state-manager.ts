80227d8f1d04f6d0b241779925a942af2fada9bb
mcabrera <manuel.cabrera@autentia.com>
2023-10-05 17:30:47 +0200

    add state manager

---
+++ src/application/state-manager.ts
@@ -0,0 +1,7 @@
+import { State } from "./state";
+import { Subject } from "../domain/observer/subject";
+
+export interface StateManager extends Subject {
+  state: State;
+  patch(state: Partial<State>): void;
+}
